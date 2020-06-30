3b9e0156852cfd19e791cab0ad36fcf2a01368ef
amisdun <michealdunamis@gmail.com>
2020-06-30 15:20:23 +0000

    added post,pul,patch to our view.py path

--- profile_api/serializers.py
+++ profile_api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
 
-class HellSerializer(serializers.Serializer):
+class HelloSerializer(serializers.Serializer):
     """A name feld for testing our APIview"""
     name = serializers.CharField(max_length=10)
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profile_api import serializers
 
 class HelloApiView(APIView):
     """Test API view"""
+    serializer_class = serializers.HelloSerializer
     def get(self,request,format=None):
         """Return a list of APIView features"""
         an_apiview = [
@@ -13,3 +16,27 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+
+            return Response({'message': message})
+
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self,request, pk=None):
+
+        return Response({'method': 'PUT'})
+
+    def patch(self,request, pk=None):
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
